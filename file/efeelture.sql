be2bde2f08c92ead5e39682c86e589139478edb8
zswin10 <1070148808@qq.com>
2017-06-05 01:42:31 +0800

    zswin10v5

--- file/efeelture.sql
+++ file/efeelture.sql
@@ -4,8 +4,8 @@
 
 DROP TABLE IF EXISTS `db_like`;
 CREATE TABLE `db_like` (
-  `lid` varchar(16) CHARACTER SET utf8mb4 NOT NULL DEFAULT '' COMMENT '点赞表的主键',
-  `uid` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '点赞表的人',
+  `lid` varchar(32) CHARACTER SET utf8mb4 NOT NULL DEFAULT '' COMMENT '点赞表的主键',
+  `uid` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '点赞表的人',
   `mid` varchar(32) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '点赞的信息',
   PRIMARY KEY (`lid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
