11ea03833136f1a2f0786a2aac29fd1ff6a432c4
zswin10 <1070148808@qq.com>
2017-06-06 01:05:30 +0800

    zswin10.0606v1

---
+++ src/util/WavUtil.java
@@ -0,0 +1,20 @@
+package util;
+
+public class WavUtil {
+
+	public static int toWav(){
+		int code=0;
+		
+		
+		return code;
+	}
+	
+	public static int saveWav(){
+		int code=0;
+		
+		
+		return code;
+	}
+	
+	
+}
