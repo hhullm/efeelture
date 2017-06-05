2202d531276e75cad7044347155f53148f68c919
zhangshu <1070148808@qq.com>
2017-06-05 16:29:10 +0800

    zsmac20170605v1

---
+++ src/util/ObjectiveUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+public class ObjectiveUtil {
+
+	public static String getPeopleNumber() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getMusic() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getWord() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getPictureNumber() {
+
+		String data="";
+		return data;
+	}
+	public static String getPictureColor() {
+
+		String data="";
+		return data;
+	}
+	
+}
---
+++ src/util/SubjectiveUtil.java
@@ -0,0 +1,34 @@
+package util;
+
+public class SubjectiveUtil {
+
+	public static String getWeather() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getTemperature() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getHumidity() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getAir() {
+
+		String data="";
+		return data;
+	}
+	
+	public static String getWeekday() {
+
+		String data="";
+		return data;
+	}
+}
