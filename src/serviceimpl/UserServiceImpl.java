02dc6a53ab8061da06b22529d09f1bf09d3e7ec2
zhangshu <1070148808@qq.com>
2017-06-06 21:25:07 +0800

    mac06062125

--- src/serviceimpl/UserServiceImpl.java
+++ src/serviceimpl/UserServiceImpl.java
@@ -5,7 +5,12 @@ package serviceimpl;
  * 
  */
 
+import java.util.HashMap;
 import java.util.Map;
+
+import com.google.gson.Gson;
+
+import entity.User;
 
 public class UserServiceImpl {
 
@@ -13,7 +18,29 @@ public class UserServiceImpl {
 	 * 注册
 	 */
 	String register(Map<String, Object> map) {
-		return null;
+		Gson j = new Gson();
+		Map<String, Object> m = new HashMap<String, Object>();
+		try {
+			UserDaoImpl userDaoImpl = new UserDaoImpl();
+			User user = MapTransEntity.transUser(map);
+			User u = userDaoImpl.checkUser(user);
+			String custId = u.getCustId();
+			if (custId != null && !custId.equals(""))
+				m.put("resultCode", "601");// 已被注册
+			else {
+				userDaoImpl.addUser(user);
+				u.setPhoneNumber(user.getPhoneNumber());
+				m.put("userList", userDaoImpl.selectUser(u));
+				m.put("resultCode", "200");
+				String headUrl = getAvatar(custId);
+				if (headUrl != null && !headUrl.equals(""))
+					m.put("headUrl", headUrl);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			m.put("resultCode", "602");// 注册失败
+		}
+		return j.toJson(m);
 	}
 
 	/*
