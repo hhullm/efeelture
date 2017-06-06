e7b3b7bd5cb50045fa5600d12b7723c391d28671
zhangshu <1070148808@qq.com>
2017-06-07 03:28:16 +0800

    air06070328

---
+++ src/utiltest/DateUtilTest.java
@@ -0,0 +1,14 @@
+package utiltest;
+
+import util.DateUtil;
+
+public class DateUtilTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String du=DateUtil.getDate();
+		System.out.println(du);
+	}
+
+}
