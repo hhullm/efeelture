53055856e0a7eb89b472ffd284de45edb49e2617
zhangshu <1070148808@qq.com>
2017-06-07 15:03:04 +0800

    air06071503

--- src/serviceimpl/CtrlServiceImpl.java
+++ src/serviceimpl/CtrlServiceImpl.java
@@ -1,7 +1,12 @@
 package serviceimpl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import com.google.gson.Gson;
+
+import daoimpl.CtrlDaoImpl;
 import entity.Ctrl;
 import service.CtrlService;
 
@@ -12,8 +17,26 @@ public class CtrlServiceImpl implements CtrlService {
 	 */
 
 	public String saveCtrl(Ctrl ctrl) {
-		return null;
-
+		Gson j = new Gson();
+		Map<String, Object> m = new HashMap<String, Object>();
+		try {
+			
+			String hid = ctrl.getHid();
+			String uid = ctrl.getUid();
+			String uipaddress = ctrl.getUipaddress();
+			if (hid == null || hid.equals("") || uid == null || uid.equals("") || uipaddress == null
+					|| uipaddress.equals(""))
+				m.put("resultCode", "111");// 缺少数据
+			else {
+				CtrlDaoImpl daoimpl = new CtrlDaoImpl();
+				daoimpl.addCtrl(ctrl);
+				m.put("resultCode", "001");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			m.put("resultCode", "110");// 注册失败
+		}
+		return j.toJson(m);
 	};
 
 	/*
