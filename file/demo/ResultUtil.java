a4b860fb4dc5c80b6e63c425c0313dff87eb35a5
zswin10 <1070148808@qq.com>
2017-06-07 23:23:32 +0800

    win06072323

--- file/demo/ResultUtil.java
+++ file/demo/ResultUtil.java
@@ -1,30 +1,42 @@
-package util;
+ackage utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
 
 public class ResultUtil {
 	public static String getResult(String resultCode) {
 		int result = Integer.valueOf(resultCode);
 		switch (result) {
-		/*
-		 * first table
-		 * second function
-		 * third 0:Exception 1~9
-		 * 
-		 * 00x true
-		 */
 		case 200:
-			return "操作成功";
+			return "鎿嶄綔鎴愬姛";
 		case 600:
-			return "未知错误600";
+			return "鏈煡閿欒";
 		case 601:
-			return "未知错误601";
+			return "鏈煡閿欒";
 		case 602:
-			return "未知错误602";
+			return "鏈煡閿欒";
 		case 603:
-			return "未知错误603";
+			return "鏈煡閿欒";
 		case 604:
-			return "未知错误604";
+			return "鏈煡閿欒";
 		default:
-			return "未知错误";
+			return "鏈煡閿欒";
 		}
+	}
+
+	public static String getResultCode() {
+		Map<String, Object> map = new HashMap<String, Object>();
+		Gson gson = new Gson();
+		map.put("resultCode", "600");//鐢ㄦ埛涓婁紶瀛楁涓嶇鍚�
+		return gson.toJson(map);
+	}
+	
+	public static String getErrorResultCode(){
+		Map<String, Object> map = new HashMap<String, Object>();
+		Gson gson = new Gson();
+		map.put("resultCode", "0000");
+		return gson.toJson(map);
 	}
 }
