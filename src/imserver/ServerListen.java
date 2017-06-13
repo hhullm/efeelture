af83999cc8a4a0fe395d7c2a6d54365f1b796a5e
zhangshu <1070148808@qq.com>
2017-06-13 21:30:03 +0800

    mac06132130

--- src/imserver/ServerListen.java
+++ src/imserver/ServerListen.java
@@ -4,17 +4,36 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
 import imclient.ClientActivity;
 
 /**
  * 客户端使用 Scoket(ip,port);参数是服务器的ip和端口号，因为没有指定
  * 客户端套接字的准确ip和端口，所以服务器发往一台机器上的任一客户端的消息，其他 客户端都能收到。所以创建套接字时，最好指定套接字端口
  */
-public class ServerListen {
+public class ServerListen implements ServletContextListener{
 	private final int PORT = 8399;
 	private ServerSocket server;
-	public static void main(String args[]){
-	   new ServerListen().begin();
+//	public static void main(String args[]){
+//	   new ServerListen().begin();
+//	}
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		try {
+			server.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+		new ServerListen().begin();
+		
 	}
 	public void begin(){
 		try {
@@ -58,4 +77,5 @@ public class ServerListen {
 	public int  size() {
 		return OnMap.getInstance().size();
 	}
+
 }
