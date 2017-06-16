767cac1f9dabe61d158b3de6545ccae79c1a4146
zhangshu <1070148808@qq.com>
2017-06-16 18:35:01 +0800

    mac06161834

--- src/imserver/ServerListen.java
+++ src/imserver/ServerListen.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 
 import imclient.ClientActivity;
 
